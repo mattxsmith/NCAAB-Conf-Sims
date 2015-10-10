d18e4039bac9847acf529a3da4168a05cd9b48eb
Matt Smith <smithmx2@gmail.com>
2015-10-10 09:25:20 -0700

    add table formatting and different champ types

--- sim.py
+++ sim.py
@@ -24,13 +24,15 @@ f.next()
 team_data = {}
 for line in f:
     d = line.split(',')
-    team_data[d[0]] = [float(d[7]), float(d[11])]
+    team_data[d[0]] = [float(d[7]), float(d[11]), int(d[14])]
 
 f.close()
 
 teams = conf_data['teams']
-team_champs = dict(zip(teams, [0]*len(teams)))
+team_champs = dict(zip(teams, [[0,0,0] for i in teams]))
 games = conf_data['schedule']
+
+wle = [[0,0,0] for i in teams]
 
 for i in range(SIMS):
     season_wins = dict(zip(teams, [0]*len(teams)))
@@ -55,6 +57,18 @@ for i in range(SIMS):
     max_wins = max([season_wins[t] for t in teams])
     champions = [t for t in teams if season_wins[t] == max_wins]
     for champ in champions:
-        team_champs[champ] += 1
+        team_champs[champ][0] += 1
+        team_champs[champ][2] += 1.0/len(champions)
+        if len(champions) == 1:
+            team_champs[champ][1] += 1
 
-print team_champs
+print('{:16} {:4} {:2} {:2} {:4}  {:6} {:6} {:6} {:}'.format('Team', 'Rnk', 'W', 'L', 'Luck', 'Share', 'Outrt', '1Seed', 'EWins'))
+
+
+for team in sorted(teams, key= lambda x: team_champs[x][0], reverse=True):
+    print '{0:15s}{1:4d}  {2:.3f}  {3:.3f}  '\
+    '{4:.3f}'.format(team, team_data[team][2], float(team_champs[team][0])/SIMS,
+        float(team_champs[team][1])/SIMS, team_champs[team][2]/SIMS)
+
+    #print('{0:16} {1:4} {2:1}  {3:1}  {4:4.1f}  {5:0.3f}  {6:0.3f}  {7:0.3f}  '\
+     #   '{8:.3}'.format(team, team_data[team][2], currentwins[ind], currentloss[ind], currentwins[ind] - exwins[ind], champs[ind]/sim, ochamps[ind]/sim, wchamps[ind]/sim, avgwins))
